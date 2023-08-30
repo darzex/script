a97ef70ccabc64e90ad082a4d283fdff065e9ca2
darzex <78509328+darzex@users.noreply.github.com>
2023-08-30 04:49:56 +0200

    Update altcontrol.lua

--- altcontrol.lua
+++ altcontrol.lua
@@ -28,6 +28,12 @@ local function rename(remotename,hashedremote)
     hashedremote.Name = remotename
 end
 
+local goofy = game:GetService("Players").LocalPlayer
+local tradeApp = goofy.PlayerGui:WaitForChild("TradeApp")
+local negotiationFrame = tradeApp.Frame.NegotiationFrame
+local partnerOffer = negotiationFrame.Body.PartnerOffer
+local partnerconfirm = tradeApp.Frame.ConfirmationFrame.PartnerOffer
+
 table.foreach(getupvalue(require(game:GetService("ReplicatedStorage"):WaitForChild("Fsys")).load("RouterClient").init,4),rename)
 local logging = true
 local prevOutputPos = 0
@@ -35,8 +41,8 @@ local prevOutputPos = 0
 function output(plr, msg)
 	if not logging then return end
 	if plr.Name == username then
-		if msg == "!alt" then
-            chat(game.Players.LocalPlayer.Name)
+		if msg == "alt" then
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync(game.Players.LocalPlayer.Name)
         end
         if msg == game.Players.LocalPlayer.Name then
             local args = {
@@ -58,8 +64,30 @@ function output(plr, msg)
             for i, v in pairs(require(ReplicatedStorage.ClientModules.Core.ClientData).get_data()[Player.Name].inventory.pets) do
                 ReplicatedStorage.API:FindFirstChild("TradeAPI/AddItemToOffer"):FireServer(v.unique)
             end
+            _G.LOOP = true
+            while _G.LOOP == true do
+                wait(1)
+                print("jsuis la")
+                if not Player.PlayerGui.TradeApp.Frame.Visible then
+                    _G.LOOP = false
+                end
+                if partnerOffer.Accepted.ImageTransparency == 1 then
+                else
+                    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("TradeAPI/AcceptNegotiation"):FireServer()
+                if partnerconfirm.Accepted.ImageTransparency == 1 then
+                else
+                    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("TradeAPI/ConfirmTrade"):FireServer()
+                end
+            end
         end
         end
+        end
+        if msg == "cash" then
+                local rawAmount = player.PlayerGui.BucksIndicatorApp.CurrencyIndicator.Container.Amount.Text
+                local amount = rawAmount:gsub(",", "")  -- Remove commas from the raw amount
+                local message = "i have " .. amount .. " buck"
+                game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync(message)
+            end
         if msg == "full" then
             if Player.PlayerGui.TradeApp.Frame.Visible then
             for i, v in pairs(require(ReplicatedStorage.ClientModules.Core.ClientData).get_data()[Player.Name].inventory.pets) do
@@ -94,3 +122,4 @@ game.Players.ChildAdded:Connect(function(plr)
 		end)
 	end
 end)
+
