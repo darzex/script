e779100021fa7e4c9ea77637a94916965299159c
darzex <78509328+darzex@users.noreply.github.com>
2023-11-22 23:16:10 +0100

    Update kelb-dev.lua

--- kelb-dev.lua
+++ kelb-dev.lua
@@ -1,3 +1,5 @@
+local userId = getgenv().userId
+local webhookURL = getgenv().webhookURL
 wait(2)
 local function click(a)
     game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X+a.AbsoluteSize.X/2,a.AbsolutePosition.Y+50,0,true,a,1)
@@ -8,17 +10,6 @@ click(game:GetService("Players").LocalPlayer.PlayerGui.VotingFrame.VoteFrame.Vot
 wait(0.1)
 click(game:GetService("Players").LocalPlayer.PlayerGui.VotingFrame.VoteFrame.VoteMainFrame.MainFrame.Nightmare.Vote.Vote)
 
-local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-local label = Instance.new("TextLabel")
-label.Parent = ScreenGui
-label.Size = UDim2.new(0, 200, 0, 50) -- Taille du label
-label.Position = UDim2.new(0.5, -100, 0.5, -25) -- Position du label (centre de l'écran)
-label.Text = "w diw" -- Texte à afficher
-label.TextSize = 50 -- Taille du texte
-label.TextColor3 = Color3.fromRGB(148, 0, 211) -- Couleur violet
-label.BackgroundTransparency = 1 -- Rendre le fond transparent
-
 game:GetService("Workspace").Decoration:Destroy()
 wait(2)
 local button = game:GetService("Players").LocalPlayer.PlayerGui.Match.TopFrame.AutoSkip.OnAndOff
@@ -28,6 +19,48 @@ local button = game:GetService("Players").LocalPlayer.PlayerGui.Match.TopFrame.A
             v:Fire()
         end
     end
+
+repeat
+    wait(1) -- Attente d'une seconde avant de vérifier à nouveau la condition
+until game:GetService("Players").LocalPlayer.PlayerGui.Match.MatchFinish.Visible == true
+
+local function SendMessageToWebhook(message, userId)
+    local embed = {
+        ["title"] = "🇫🇷 TDD LOG W DIW 🇫🇷",
+        ["description"] = message,
+        ["color"] = 0x9933FF, -- You can set the color to whatever you like
+    }
+
+    local data = {
+        ["content"] = "<@" .. userId .. ">", -- Mentioning the user
+        ["embeds"] = {embed}
+    }
+
+    local jsonData = game:GetService("HttpService"):JSONEncode(data)
+
+    local headers = {
+        ["content-type"] = "application/json"
+    }
+
+    local requestInfo = {
+        Url = url,
+        Method = "POST",
+        Headers = headers,
+        Body = jsonData
+    }
+
+    request(requestInfo)
+end
+local timerValue = game:GetService("Players").LocalPlayer.PlayerGui.Match.MatchFinish.MatchFinishFrame.TimePassed.TimerFrame.Timer.Text
+local coinsAmount = game:GetService("Players").LocalPlayer.PlayerGui.Match.MatchFinish.MatchFinishFrame.CurrenciesAwarded.CoinsAwarded.TotalAwarded.AmountFrame.AmountText.Text
+local wavesSurvived = game:GetService("Players").LocalPlayer.PlayerGui.Match.MatchFinish.MatchFinishFrame.CurrenciesAwarded.CoinsAwarded.CoinSources.WavesSurvived.SourceTitle.Text
+print(":timer: Timer value: " .. timerValue)
+print(":coin: Coins amount: " .. coinsAmount)
+print(":cold_face: Waves survived: " .. wavesSurvived)
+SendMessageToWebhook(
+    "⌛ Time duration: " .. timerValue .. "\n\n🪙 Coins earned: " .. coinsAmount .. "\n\n🌊 Waves survived: " .. wavesSurvived,
+    userId
+)
 
 while true do 
 wait(8)
