f4aa36b8feb633af07e1c5e8d567a49f9e1e4918
darzex <78509328+darzex@users.noreply.github.com>
2023-11-22 21:18:27 +0100

    Update kelb-dev.lua

--- kelb-dev.lua
+++ kelb-dev.lua
@@ -8,6 +8,17 @@ click(game:GetService("Players").LocalPlayer.PlayerGui.VotingFrame.VoteFrame.Vot
 wait(0.1)
 click(game:GetService("Players").LocalPlayer.PlayerGui.VotingFrame.VoteFrame.VoteMainFrame.MainFrame.Nightmare.Vote.Vote)
 
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+local label = Instance.new("TextLabel")
+label.Parent = ScreenGui
+label.Size = UDim2.new(0, 200, 0, 50) -- Taille du label
+label.Position = UDim2.new(0.5, -100, 0.5, -25) -- Position du label (centre de l'écran)
+label.Text = "w diw" -- Texte à afficher
+label.TextSize = 50 -- Taille du texte
+label.TextColor3 = Color3.fromRGB(148, 0, 211) -- Couleur violet
+label.BackgroundTransparency = 1 -- Rendre le fond transparent
+
 game:GetService("Workspace").Decoration:Destroy()
 wait(2)
 local button = game:GetService("Players").LocalPlayer.PlayerGui.Match.TopFrame.AutoSkip.OnAndOff
