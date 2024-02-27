b7d66c7d3e3e3e7f5ac0f4fd22c983be574f9a06
darzex <78509328+darzex@users.noreply.github.com>
2024-02-28 00:51:57 +0100

    Update adp-farm.lua

--- adp-farm.lua
+++ adp-farm.lua
@@ -1,7 +1,5 @@
 repeat wait() until game:IsLoaded()
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 task.wait(1)
-setfpscap(3)
-task.wait(1)
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/ReQiuYTPL/hub/main/ReQiuYTPLHub.lua'),true))()
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMae17/NewIceHub/main/Brookhaven"))()
