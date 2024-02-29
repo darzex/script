7560c3cafa24f8054b540a125a039b24d67b1463
darzex <78509328+darzex@users.noreply.github.com>
2024-02-29 22:45:01 +0100

    Update adp-farm.lua

--- adp-farm.lua
+++ adp-farm.lua
@@ -1,3 +1,3 @@
 repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/ReQiuYTPL/hub/main/ReQiuYTPLHub.lua'),true))()
---loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMae17/NewIceHub/main/Brookhaven"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMae17/NewIceHub/main/Brookhaven"))()
